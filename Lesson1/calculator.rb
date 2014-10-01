6e5348e09d22d7538af702874e5bd2f35c2d9b55
iheartkode <y2kmark73@gmail.com>
2014-09-30 18:51:38 -0700

    Adding Lesson1 directory and calculator

---
+++ Lesson1/calculator.rb
@@ -0,0 +1,27 @@
+#Calculator App
+require 'pry'
+
+def say(msg)
+  puts "::::: #{msg} :::::"
+end
+
+say "What's the first number?"
+num1 = gets.chomp
+
+say "What's the second number?"
+num2 = gets.chomp
+
+say "1) add 2) subtract 3) multiply 4) division"
+operator = gets.chomp
+
+if operator == '1'
+  result = num1.to_i + num2.to_i
+elsif operator == '2'
+  result = num1.to_i - num2.to_i
+elseif operator == '3'
+  result = num1.to_i * num2.to_i
+else operator == '4'
+  result = num1.to_f / num2.to_f
+end
+
+puts "Result is #{result}"
