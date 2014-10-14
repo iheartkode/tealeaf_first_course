d17ce1c77b702c36ab11e25886ea0c3d89ff05de
iheartkode <y2kmark73@gmail.com>
2014-10-14 16:34:08 -0700

    Added prs

--- Lesson1/random_quote.rb
+++
@@ -1,7 +0,0 @@
-#Random_quote side app
-
-quotes = ["You only die once!", "If all else fails.."]
-def prompt(msg)
-  puts ">" msg
-end
-
--- Lesson1/rock_paper_scissors.rb
+++ Lesson1/rock_paper_scissors.rb
@@ -1,16 +1,60 @@
+# Paper Rock Scissos
+#Mark Howard
+#Tealeaf Academy Lesson 1
+
+#Method to make prompt
 def prompt(msg)
-  puts "=> #{msg}"
+  puts "[] #{msg}"
 end
 
-CHOICES {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+prompt("::::PAPER ROCK SCISSOR:::")
+prompt("Begin...")
 
-prompt("Welcome to paper, rock , scissors")
+#A method for the game logic
+def start_game
 
-loop do
-  begin
-    prompt("Choose one: (P/R/S)")
-    choice = gets.chomp
-  end until CHOICES.keys.include?(choice)
-  prompt("Let's Play!")
+ai = ['R','P','S'].sample
 
+prompt("Choose paper(p), rock(r) or scissor(s)")
+human = gets.chomp.upcase
+
+  case
+  when human == "P" && ai == "P"
+    prompt("You chose Paper and I chose Paper: Tie")
+  when human == "R" && ai == "R"
+    prompt("You chose Rock and I chose Rock: Tie")
+  when human == "S" && ai == "S"
+    prompt("You chose Scissors and I chose Scissors: Tie")
+  when  human == "P"  &&  ai == "R"
+    prompt("You chose paper and I chose Rock, you win!")
+  when human == "R" && ai == "S"
+    prompt("You chose Rock and I chose Scissors, You win!")
+  when human == "S" && ai == "P"
+    prompt("You chose Scissors and I chose Paper, You win!")
+  when human == "P" && ai == "R"
+    prompt("You chose Paper and I chose Rock, You win! ")
+  when human == "S" && ai == "R"
+    prompt("You chose Scissors and I chose Rock, I win!")
+  when human == "P" && ai == "S"
+    prompt("You chose Paper and I chose Scissors, I win")
+  when human == "R" && ai == "P"
+    prompt("You chose Rock and I chose Paper, I win!")
+  end
 end
+
+start_game
+
+prompt("Would you like to play again?")
+answer = gets.chomp.upcase
+
+if answer == "YES"
+  prompt("loading...")
+  sleep 1
+  start_game
+else
+  prompt("Oh, you must be scared...")
+end
+
+
+
+
